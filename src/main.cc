7d67c89cbcd0b22d593829112b6a38824be6ff8c
Morten Kristensen <msk@nullpointer.dk>
2016-09-14 22:38:55 +0200

    main also shows info on all binary objects.

--- src/main.cc
+++ src/main.cc
@@ -12,8 +12,22 @@ int main(int argc, char **argv) {
 
   auto fmt = Format::detect(file);
   if (!fmt) return -1;
-
   qDebug() << "format:" << Util::formatTypeString(fmt->getType());
+
+  qDebug() << "parsing..";
+  bool ret = fmt->parse();
+  if (!ret) return -1;
+
+  const auto &objects = fmt->getObjects();
+  qDebug() << "#objects:" << objects.size();
+
+  for (const auto &object : objects) {
+    qDebug() << "===";
+    qDebug() << "cpu type:" << Util::cpuTypeString(object->getCpuType());
+    qDebug() << "cpu sub type:" << Util::cpuTypeString(object->getCpuSubType());
+    qDebug() << "little endian:" << object->isLittleEndian();
+    qDebug() << "file type:" << Util::fileTypeString(object->getFileType());
+  }
 
   return 0;
 }
