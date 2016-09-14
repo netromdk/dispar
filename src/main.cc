9ce3e886abe08b21c2872215530907fe96de0966
Morten Kristensen <msk@nullpointer.dk>
2016-09-14 22:31:33 +0200

    Simple main that detects format.

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,19 @@
+#include <QString>
+#include <QDebug>
+
+#include "Util.h"
+#include "formats/Format.h"
+
 int main(int argc, char **argv) {
+  // Temporary!
+  if (argc != 2) return -1;
+  auto file = QString::fromUtf8(argv[1]);
+  qDebug() << "input:" << file;
+
+  auto fmt = Format::detect(file);
+  if (!fmt) return -1;
+
+  qDebug() << "format:" << Util::formatTypeString(fmt->getType());
+
   return 0;
 }
