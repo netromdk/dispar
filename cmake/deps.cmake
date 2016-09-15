22039228b30f503b4974ecf63c308512d12fb575
Morten Kristensen <msk@nullpointer.dk>
2016-09-16 00:02:19 +0200

    AUTORCC and added Windows Qt paths.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,16 +1,21 @@
 # Qt 5
+# Added some paths to help find the modules on some systems.
 if (NOT WIN32)
-  # Added some paths to help find the modules on some systems.
-  set(CMAKE_PREFIX_PATH
-    ${CMAKE_PREFIX_PATH}
+  set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH}
     "/usr/local/opt"
     "/usr/local/opt/qt5"
     "/usr/local/Qt-5.6.0"
     "/usr/local/Qt-5.4.1"
     )
+else()
+  set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH}
+    "C:/qt-everywhere-opensource-src-5.6.0-w64/qtbase/lib/cmake"
+    "C:/qt-everywhere-opensource-src-5.4.1-w64/qtbase/lib/cmake"
+    )
 endif()
 
-set(CMAKE_AUTOMOC ON) # Moc when necessary.
+set(CMAKE_AUTOMOC ON) # moc when necessary.
+set(CMAKE_AUTORCC ON) # rcc when necessary.
 
 # As moc files are generated in the binary dir, tell CMake to always look for includes there:
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
