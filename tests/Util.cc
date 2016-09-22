6a7d5c68eb7d8f021153ae2cd0cde75fa11d33a5
Morten Kristensen <msk@nullpointer.dk>
2016-09-22 21:59:00 +0200

    Testing Util::demangle().

---
+++ tests/Util.cc
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+#include "testutils.h"
+
+#include "Util.h"
+
+TEST(Util, demangle)
+{
+  auto res = Util::demangle("__ZSt9terminatev");
+  EXPECT_EQ(res, "std::terminate()") << res;
+
+  res = Util::demangle("__ZNK9QComboBox8findDataERK8QVarianti6QFlagsIN2Qt9MatchFlagEE");
+  EXPECT_EQ(res, "QComboBox::findData(QVariant const&, int, QFlags<Qt::MatchFlag>) const") << res;
+
+  res = Util::demangle("__ZdlPv");
+  EXPECT_EQ(res, "operator delete(void*)") << res;
+
+  res = Util::demangle("__Znam");
+  EXPECT_EQ(res, "operator new[](unsigned long)") << res;
+}
