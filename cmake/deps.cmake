9e33bf7a8fc11c74a01fc99d53ae32a95e733572
Morten Kristensen <msk@nullpointer.dk>
2016-09-23 23:25:00 +0200

    Qt deps path order.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -2,10 +2,11 @@
 # Added some paths to help find the modules on some systems.
 if (NOT WIN32)
   set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH}
-    "/usr/local/opt"
-    "/usr/local/opt/qt5"
     "/usr/local/Qt-5.6.0"
     "/usr/local/Qt-5.4.1"
+    "/usr/local/Qt-5.4.0"
+    "/usr/local/opt"
+    "/usr/local/opt/qt5"
     )
 else()
   set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH}
