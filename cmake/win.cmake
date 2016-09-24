a10b66cc32fdd185d2803f5cf6ceeaf2741a10e9
Morten Kristensen <morten@luxion.com>
2016-09-24 17:18:38 +0200

    Prepare Windows executables.

--- cmake/win.cmake
+++ cmake/win.cmake
@@ -2,4 +2,13 @@ add_definitions(
   -DWIN
   -D_USE_MATH_DEFINES
   -DNOMINMAX
+  -D_UNICODE
+  -DUNICODE
   )
+
+macro(prepare_win_executable target)
+  if (WIN32)
+    append_target_properties(${target} LINK_FLAGS_DEBUG "/subsystem:console")
+    append_target_properties(${target} LINK_FLAGS "/ENTRY:\"mainCRTStartup\"")
+  endif()
+endmacro()
