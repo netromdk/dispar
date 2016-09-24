0440fe73a3d6cfa932c083e6c891d37efd0a41d8
Morten Kristensen <morten@luxion.com>
2016-09-24 16:18:50 +0200

    Capstone requires CAPSTONE_INC_DIR and CAPSTONE_STATIC_LIB.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -12,6 +12,7 @@ else()
   set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH}
     "C:/qt-everywhere-opensource-src-5.6.0-w64/qtbase/lib/cmake"
     "C:/qt-everywhere-opensource-src-5.4.1-w64/qtbase/lib/cmake"
+    "C:/qt-everywhere-opensource-src-5.4.0-w64/qtbase/lib/cmake"
     )
 endif()
 
@@ -25,16 +26,18 @@ find_package(Qt5Core REQUIRED)
 find_package(Qt5Gui REQUIRED)
 find_package(Qt5Widgets REQUIRED)
 
+set(FOUND_QT YES)
+
 # Capstone disassembler
-if (NOT WIN32)
+if (NOT WIN32 AND NOT CAPSTONE_INC_DIR AND NOT CAPSTONE_STATIC_LIB)
   set(CAPSTONE_INC_DIR "/usr/local/include/capstone")
   set(CAPSTONE_STATIC_LIB "/usr/local/lib/libcapstone.a")
-  if (NOT EXISTS ${CAPSTONE_INC_DIR})
-    message(FATAL_ERROR "Could not detect Capstone include dir: ${CAPSTONE_INC_DIR}")
-  endif()
-  if (NOT EXISTS ${CAPSTONE_STATIC_LIB})
-    message(FATAL_ERROR "Could not detect Capstone static library: ${CAPSTONE_STATIC_LIB}")
-  endif()
-else()
-  message(FATAL_ERROR "Detection of Capstone for Windows not implemented yet!")
 endif()
+set(FOUND_CAPSTONE NO)
+if (NOT EXISTS ${CAPSTONE_INC_DIR})
+  message(FATAL_ERROR "Could not detect Capstone include dir: ${CAPSTONE_INC_DIR}")
+endif()
+if (NOT EXISTS ${CAPSTONE_STATIC_LIB})
+  message(FATAL_ERROR "Could not detect Capstone static library: ${CAPSTONE_STATIC_LIB}")
+endif()
+set(FOUND_CAPSTONE YES)
