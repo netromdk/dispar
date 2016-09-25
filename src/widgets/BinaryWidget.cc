6a2604290bcad0ead62622e195f44b8af34005f9
Morten Kristensen <msk@nullpointer.dk>
2016-09-25 11:41:21 +0200

    Center on scroll.

--- src/widgets/BinaryWidget.cc
+++ src/widgets/BinaryWidget.cc
@@ -84,6 +84,7 @@ void BinaryWidget::createLayout()
 
   mainView = new QPlainTextEdit;
   mainView->setReadOnly(true);
+  mainView->setCenterOnScroll(true);
   mainView->setLineWrapMode(QPlainTextEdit::NoWrap);
   mainView->setFont(QFontDatabase::systemFont(QFontDatabase::FixedFont));
   connect(mainView, &QPlainTextEdit::cursorPositionChanged, this,
