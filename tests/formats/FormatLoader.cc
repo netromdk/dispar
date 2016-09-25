a5363eb8c75dd7bdff8a0891b7b1bb742364ce80
Morten Kristensen <msk@nullpointer.dk>
2016-09-25 18:37:29 +0200

    100% coverage of FormatLoader.

--- tests/formats/FormatLoader.cc
+++ tests/formats/FormatLoader.cc
@@ -4,8 +4,6 @@
 
 #include "formats/FormatLoader.h"
 
-// TODO: create general create function with variadic templates instead of one(), two()..
-
 TEST(FormatLoader, failedNonexistent)
 {
   FormatLoader loader("_/something/that/does/not/exist");
@@ -19,9 +17,32 @@ TEST(FormatLoader, failedNonexistent)
   failedSpy->wait();
 }
 
-TEST(FormatLoader, failedCouldNotParse)
+TEST(FormatLoader, failedCouldNotDetect)
 {
   auto file = tempFile();
+  FormatLoader loader(file->fileName());
+
+  auto failedSpy = SIGNAL_SPY_ONE(const QString &, &loader, &FormatLoader::failed);
+
+  auto successSpy = SIGNAL_SPY_ONE(std::shared_ptr<Format>, &loader, &FormatLoader::success);
+  successSpy->setExpect(false);
+
+  loader.start();
+  failedSpy->wait();
+}
+
+TEST(FormatLoader, failedCouldNotParse)
+{
+  // This means having a correct magic code but invalid binary otherwise.
+  // Put MachO magic code: 0xFEEDFACE
+
+  QByteArray data;
+  data.append(0xCE);
+  data.append(0xFA);
+  data.append(0xED);
+  data.append(0xFE);
+
+  auto file = tempFile(data);
   FormatLoader loader(file->fileName());
 
   auto failedSpy = SIGNAL_SPY_ONE(const QString &, &loader, &FormatLoader::failed);
