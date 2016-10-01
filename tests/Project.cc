610a9224dfa61f4b21759c253bf4799a6396402b
Morten Kristensen <msk@nullpointer.dk>
2016-10-01 20:09:56 +0200

    More Project testing.

--- tests/Project.cc
+++ tests/Project.cc
@@ -20,6 +20,21 @@ TEST(Project, save)
   EXPECT_EQ(p.file(), path) << p.file();
 }
 
+TEST(Project, saveHasFileAlready)
+{
+  Project p;
+  p.setBinary("something.o");
+
+  auto file = tempFile();
+  auto path = file->fileName();
+  ASSERT_TRUE(p.save(path)) << "Could not save to: " << path;
+  EXPECT_EQ(p.file(), path) << p.file();
+
+  p.setBinary("another.dylib");
+  ASSERT_TRUE(p.save());
+  EXPECT_EQ(p.file(), path) << p.file();
+}
+
 TEST(Project, load)
 {
   Project p;
@@ -28,6 +43,23 @@ TEST(Project, load)
   auto file = tempFile();
   auto path = file->fileName();
   ASSERT_TRUE(p.save(path)) << "Could not save to: " << path;
+
+  auto p2 = Project::load(path);
+  ASSERT_NE(p2, nullptr) << "Could not load from: " << path;
+  EXPECT_EQ(p2->binary(), p.binary()) << p2->binary() << p.binary();
+}
+
+TEST(Project, loadFromAlreadyKnownFile)
+{
+  Project p;
+  p.setBinary("something.o");
+
+  auto file = tempFile();
+  auto path = file->fileName();
+  ASSERT_TRUE(p.save(path)) << "Could not save to: " << path;
+
+  // Make sure the values are correctly saved when giving no arguments.
+  ASSERT_TRUE(p.save());
 
   auto p2 = Project::load(path);
   ASSERT_NE(p2, nullptr) << "Could not load from: " << path;
