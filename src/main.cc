c08725ede101d8a21453b8050f9e8139f6637e88
Morten Kristensen <msk@nullpointer.dk>
2016-10-01 13:24:54 +0200

    Register software signals SIGINt, SIGABRT, and SIGTERM.

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,21 @@
 #include <QApplication>
 #include <QCommandLineParser>
+#include <QDebug>
 #include <QStringList>
 #include <QTimer>
+
+#include <signal.h>
 
 #include "Context.h"
 #include "Version.h"
 #include "formats/Format.h"
 #include "widgets/MainWindow.h"
+
+void signalHandler(int sig)
+{
+  qDebug().nospace() << "Caught software signal " << sig << ". Cleaning up and closing down.";
+  QApplication::exit(0);
+}
 
 int main(int argc, char **argv)
 {
@@ -14,7 +23,10 @@ int main(int argc, char **argv)
   app.setApplicationName("Dispar");
   app.setApplicationVersion(versionString());
 
-  Format::registerType();
+  // Register software signals.
+  signal(SIGINT, &signalHandler);
+  signal(SIGABRT, &signalHandler);
+  signal(SIGTERM, &signalHandler);
 
   QCommandLineParser parser;
   parser.addHelpOption();
@@ -28,6 +40,9 @@ int main(int argc, char **argv)
   if (posArgs.size() == 1) {
     file = posArgs.first();
   }
+
+  // Register meta types.
+  Format::registerType();
 
   // Initialize and load context.
   Context::get();
