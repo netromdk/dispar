5da37a7047797d549d5baabb72c6b704c829cd9b
Morten Kristensen <msk@nullpointer.dk>
2016-10-02 13:42:11 +0200

    Context loads/saves JSON.

--- src/Context.cc
+++ src/Context.cc
@@ -2,10 +2,22 @@
 #include "Project.h"
 
 #include <QDebug>
+#include <QDir>
 #include <QFile>
-#include <QSettings>
+#include <QJsonDocument>
+#include <QJsonObject>
 
-Context::Context() : project_(nullptr)
+namespace {
+
+QString settingsPath()
+{
+  return QDir::home().absoluteFilePath(".dispar");
+}
+
+} // anon
+
+Context::Context()
+  : showMachineCode_(true), disassemblerSyntax_(Disassembler::Syntax::INTEL), project_(nullptr)
 {
   loadSettings();
 }
@@ -47,20 +59,50 @@ void Context::setDisassemblerSyntax(Disassembler::Syntax syntax)
 
 void Context::loadSettings()
 {
-  qDebug() << "Loading settings";
-  QSettings settings;
-  showMachineCode_ = settings.value("Context.showMachineCode", true).toBool();
-  disassemblerSyntax_ = static_cast<Disassembler::Syntax>(
-    settings.value("Context.disassemblerSyntax", static_cast<int>(Disassembler::Syntax::INTEL))
-      .toInt());
+  auto path = settingsPath();
+  qDebug() << "Loading settings from" << path;
+
+  QFile file(path);
+  if (!file.open(QIODevice::ReadOnly)) {
+    qCritical() << "Could not read settings from" << path;
+    return;
+  }
+
+  auto doc = QJsonDocument::fromJson(file.readAll());
+  if (doc.isNull() || doc.isEmpty() || !doc.isObject()) {
+    qCritical() << "Malformed or empty settings file!";
+    return;
+  }
+
+  auto obj = doc.object();
+  if (obj.contains("showMachineCode")) {
+    showMachineCode_ = obj["showMachineCode"].toBool(true);
+  }
+  if (obj.contains("disassemblerSyntax")) {
+    disassemblerSyntax_ = static_cast<Disassembler::Syntax>(
+      obj["disassemblerSyntax"].toInt(static_cast<int>(Disassembler::Syntax::INTEL)));
+  }
 }
 
 void Context::saveSettings()
 {
-  qDebug() << "Saving settings";
-  QSettings settings;
-  settings.setValue("Context.showMachineCode", showMachineCode());
-  settings.setValue("Context.disassemblerSyntax", static_cast<int>(disassemblerSyntax()));
+  auto path = settingsPath();
+  qDebug() << "Saving settings to" << path;
+
+  QJsonObject obj;
+  obj["showMachineCode"] = showMachineCode();
+  obj["disassemblerSyntax"] = static_cast<int>(disassemblerSyntax());
+
+  QJsonDocument doc;
+  doc.setObject(obj);
+
+  auto data = doc.toJson();
+  QFile file(path);
+  if (!file.open(QIODevice::WriteOnly)) {
+    qCritical() << "Could not write settings to" << path;
+    return;
+  }
+  file.write(data);
 }
 
 std::shared_ptr<Project> Context::project() const
