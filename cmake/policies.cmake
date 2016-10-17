a0acd24b76c6bdf0b784a218474cbce99903dc24
Morten Kristensen <msk@nullpointer.dk>
2016-10-17 20:44:08 +0200

    Cmake: Guard policies if not known to older versions.

--- cmake/policies.cmake
+++ cmake/policies.cmake
@@ -1,7 +1,11 @@
 # Only interpret if() arguments as variables or keywords when unquoted.
-cmake_policy(SET CMP0054 NEW)
+if (CMP0054)
+  cmake_policy(SET CMP0054 NEW)
+endif()
 
 # Target names should not be reserved and should match a validity pattern. Target names reserved by
 # one or more CMake generators are not allowed. Among others these include "all", "help" and
 # "test".
-cmake_policy(SET CMP0037 NEW)
+if (CMP0037)
+  cmake_policy(SET CMP0037 NEW)
+endif()
