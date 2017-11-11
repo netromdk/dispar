2f0a6cca37bb03a3987848024545379a40be99e4
Morten Kristensen <msk@nullpointer.dk>
2017-11-11 18:42:55 +0100

    SA less dependant on custom setup

--- scripts/static_analysis.sh
+++ scripts/static_analysis.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-BUILD_FLD=$1
-QT_DIR=$2
-TARGET=$3
+SCAN_BUILD=$1
+BUILD_FLD=$2
+QT_DIR=$3
+TARGET=$4
 
 # Hack to make ccc-analyzer find Qt includes properly!
 rm -f ${BUILD_FLD}/src/QtCore ${BUILD_FLD}/src/QtGui ${BUILD_FLD}/src/QtWidgets
@@ -9,4 +10,4 @@ ln -s ${QT_DIR}/QtCore.framework/Headers/ ${BUILD_FLD}/src/QtCore
 ln -s ${QT_DIR}/QtGui.framework/Headers/ ${BUILD_FLD}/src/QtGui
 ln -s ${QT_DIR}/QtWidgets.framework/Headers/ ${BUILD_FLD}/src/QtWidgets
 
-scan-build -v -V -k ninja ${TARGET}
+${SCAN_BUILD} -v -V -k cmake --build ${BUILD_FLD} --target ${TARGET}
