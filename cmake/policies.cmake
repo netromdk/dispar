c308b4f4bf03694bfc97c6aaf29fad9e92e5c028
Morten Kristensen <msk@nullpointer.dk>
2017-11-11 09:22:53 +0100

    Cmake: Add policy CMP0025 to fix behavior with CMAKE_CXX_STANDARD for macOS

--- cmake/policies.cmake
+++ cmake/policies.cmake
@@ -1,11 +1,16 @@
 # Only interpret if() arguments as variables or keywords when unquoted.
-if (CMP0054)
+if (POLICY CMP0054)
   cmake_policy(SET CMP0054 NEW)
 endif()
 
 # Target names should not be reserved and should match a validity pattern. Target names reserved by
 # one or more CMake generators are not allowed. Among others these include "all", "help" and
 # "test".
-if (CMP0037)
+if (POLICY CMP0037)
   cmake_policy(SET CMP0037 NEW)
+endif()
+
+# Fix behavior of CMAKE_CXX_STANDARD when targeting macOS.
+if (POLICY CMP0025)
+  cmake_policy(SET CMP0025 NEW)
 endif()
