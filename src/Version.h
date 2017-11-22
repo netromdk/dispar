e34e476d4d01ef0f4c1891043b8458f762af4ee4
Morten Kristensen <msk@nullpointer.dk>
2017-11-23 00:03:42 +0100

    Version 0.2

--- src/Version.h
+++ src/Version.h
@@ -2,26 +2,22 @@
 #define DISPAR_VERSION_H
 
 #include <QString>
-#include <QStringList>
+#include <QVersionNumber>
 
 #define MAJOR_VERSION 0
-#define MINOR_VERSION 1
-#define BUILD_VERSION 0
+#define MINOR_VERSION 2
 
-#define BUILD_DATE "September 14, 2016"
+#define VERSION_DATE "November 23, 2017"
 
-#define MAJOR_FACTOR 1000000
-#define MINOR_FACTOR 1000
-
-static inline QString versionString(int major, int minor, int build, bool showDate = false)
+static inline QString versionString(int major, int minor, bool showDate = false)
 {
-  return QString::number(major) + "." + QString::number(minor) + "." + QString::number(build) +
-         (showDate ? " [" + QString(BUILD_DATE) + "]" : "");
+  return QVersionNumber({major, minor}).toString() +
+         (showDate ? " [" + QString(VERSION_DATE) + "]" : "");
 }
 
 static inline QString versionString(bool showDate = false)
 {
-  return versionString(MAJOR_VERSION, MINOR_VERSION, BUILD_VERSION, showDate);
+  return versionString(MAJOR_VERSION, MINOR_VERSION, showDate);
 }
 
 #endif // DISPAR_VERSION_H
