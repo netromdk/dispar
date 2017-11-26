522b53c853d6110bb341d39bf3bc45879e7d0485
Morten Kristensen <msk@nullpointer.dk>
2017-11-26 16:38:38 +0100

    Using constants instead of #defines

--- src/Version.h
+++ src/Version.h
@@ -4,12 +4,11 @@
 #include <QString>
 #include <QVersionNumber>
 
-// TODO: MAke thede defines into static constexpr!
-#define MAJOR_VERSION 0
-#define MINOR_VERSION 2
-#define VERSION_DATE "November 23, 2017"
-
 namespace dispar {
+
+static constexpr int MAJOR_VERSION = 0;
+static constexpr int MINOR_VERSION = 2;
+static const QString VERSION_DATE("November 23, 2017");
 
 static inline QString versionString(int major, int minor, bool showDate = false)
 {
