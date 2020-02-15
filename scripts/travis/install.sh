600bf5ac1e60b8dee257652f999cdfa1c6639196
Morten Kristensen <me@mortens.dev>
2020-02-15 16:50:39 +0100

    [travis] Use Xcode 11.3 and macOS 10.14

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -5,10 +5,10 @@ set -x
 
 # Linux
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-    sudo apt-get install -qq qt514base mesa-common-dev libglu1-mesa-dev
+  sudo apt-get install -qq qt514base mesa-common-dev libglu1-mesa-dev
 fi
 
 # macOS
 if [[ $TRAVIS_OS_NAME == "osx" ]]; then
-    brew install -v qt5
+  brew install -v qt5
 fi
