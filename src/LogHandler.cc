2e7ed7f1a6ab717707bd3d2d8b9566818ac5f737
Morten Kristensen <me@mortens.dev>
2020-03-03 22:55:42 +0100

    LogHandler keeps track of entries and signals new ones

--- src/LogHandler.cc
+++ src/LogHandler.cc
@@ -1,5 +1,8 @@
 #include "LogHandler.h"
 #include "Context.h"
+#include "cxx.h"
+
+#include <cassert>
 
 #include <QDir>
 #include <QFileInfo>
@@ -11,15 +14,43 @@ namespace dispar {
 
 namespace {
 
-// Make context available in static messageHandler().
+// Make context and handler available in static messageHandler().
 Context *ctx = nullptr;
+LogHandler *instance = nullptr;
 
 } // namespace
 
+QString LogHandler::Entry::typeString() const
+{
+  switch (type) {
+  case QtDebugMsg:
+    return tr("Debug");
+  default:
+  case QtInfoMsg:
+    return tr("Info");
+  case QtWarningMsg:
+    return tr("Warning");
+  case QtCriticalMsg:
+    return tr("Critical");
+  case QtFatalMsg:
+    return tr("Fatal");
+  }
+}
+
 LogHandler::LogHandler(Context &context)
 {
+  ASSERT_X(!instance, "Only one LogHandler can be live at any one time");
+  instance = this;
+
   ctx = &context;
+
   qInstallMessageHandler(this->messageHandler);
+}
+
+LogHandler::~LogHandler()
+{
+  assert(instance);
+  instance = nullptr;
 }
 
 void LogHandler::messageHandler(QtMsgType type, const QMessageLogContext &context,
@@ -59,13 +90,30 @@ void LogHandler::messageHandler(QtMsgType type, const QMessageLogContext &contex
 
   // Lock mutex while writing to stdout such that output isn't messed up when coming from multiple
   // threads.
-  QMutexLocker locker(&mutex);
+  {
+    QMutexLocker locker(&mutex);
 
-  QTextStream stream(stdout);
-  stream << output;
-  if (!output.endsWith("\n")) {
-    stream << "\n";
+    QTextStream stream(stdout);
+    stream << output;
+    if (!output.endsWith("\n")) {
+      stream << "\n";
+    }
   }
+
+  if (instance) {
+    instance->addEntry({QDateTime::currentDateTime(), type, msg});
+  }
+}
+
+void LogHandler::addEntry(const Entry &entry)
+{
+  entries_ << entry;
+  emit newEntry(entry);
+}
+
+QList<LogHandler::Entry> LogHandler::entries() const
+{
+  return entries_;
 }
 
 } // namespace dispar
--- src/LogHandler.h
+++ src/LogHandler.h
@@ -1,18 +1,41 @@
 #ifndef SRC_LOGHANDLER_H
 #define SRC_LOGHANDLER_H
 
+#include <QDateTime>
 #include <QDebug>
+#include <QObject>
 
 namespace dispar {
 
 class Context;
 
-class LogHandler {
+class LogHandler : public QObject {
+  Q_OBJECT
+
 public:
+  struct Entry {
+    QDateTime time;
+    QtMsgType type = QtMsgType::QtInfoMsg;
+    QString msg;
+
+    QString typeString() const;
+    // TODO: add context perhaps..
+  };
+
   LogHandler(Context &context);
+  virtual ~LogHandler();
 
   /// Message handler to be installed via \p qInstallMessageHandler().
   static void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
+
+  void addEntry(const Entry &entry);
+  QList<Entry> entries() const;
+
+signals:
+  void newEntry(const Entry &entry);
+
+private:
+  QList<Entry> entries_;
 };
 
 } // namespace dispar
