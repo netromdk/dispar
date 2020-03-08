d1228d5aa9c2ab46f311644302576476e613323d
Morten Kristensen <me@mortens.dev>
2020-03-08 08:53:59 +0100

    Restructured Disassembler tests

--- tests/Disassembler.cc
+++ tests/Disassembler.cc
@@ -26,121 +26,121 @@ TEST(Disassembler, valid)
   }
 }
 
-TEST(Disassembler, disassembleData)
+TEST(Disassembler, disassembleData_Intel_32_x86)
 {
-  {
-    // x86 32-bit
-    auto obj = std::make_unique<BinaryObject>();
-    Disassembler dis(*obj.get(), Disassembler::Syntax::INTEL);
-    ASSERT_TRUE(dis.valid());
+  auto obj = std::make_unique<BinaryObject>();
+  obj->setCpuType(CpuType::X86);
 
-    // dec eax
-    // sub esp, 0x70
-    const char *code = "\x48\x83\xec\x70";
-    auto res = dis.disassemble(QByteArray(code));
-    ASSERT_NE(res, nullptr);
-    EXPECT_EQ(res->count(), std::size_t(2));
+  Disassembler dis(*obj.get(), Disassembler::Syntax::INTEL);
+  ASSERT_TRUE(dis.valid());
 
-    auto *instr = res->instructions(0);
-    ASSERT_NE(instr, nullptr);
-    EXPECT_EQ(std::string(instr->mnemonic), std::string("dec")) << instr->mnemonic;
-    EXPECT_EQ(std::string(instr->op_str), std::string("eax")) << instr->op_str;
+  // dec eax
+  // sub esp, 0x70
+  const char *code = "\x48\x83\xec\x70";
+  auto res = dis.disassemble(QByteArray(code));
+  ASSERT_NE(res, nullptr);
+  EXPECT_EQ(res->count(), std::size_t(2));
 
-    instr = res->instructions(1);
-    ASSERT_NE(instr, nullptr);
-    EXPECT_EQ(std::string(instr->mnemonic), std::string("sub")) << instr->mnemonic;
-    EXPECT_EQ(std::string(instr->op_str), std::string("esp, 0x70")) << instr->op_str;
-  }
+  auto *instr = res->instructions(0);
+  ASSERT_NE(instr, nullptr);
+  EXPECT_EQ(std::string(instr->mnemonic), std::string("dec")) << instr->mnemonic;
+  EXPECT_EQ(std::string(instr->op_str), std::string("eax")) << instr->op_str;
 
-  {
-    // x86 32-bit
-    auto obj = std::make_unique<BinaryObject>();
-    Disassembler dis(*obj.get(), Disassembler::Syntax::ATT);
-    ASSERT_TRUE(dis.valid());
+  instr = res->instructions(1);
+  ASSERT_NE(instr, nullptr);
+  EXPECT_EQ(std::string(instr->mnemonic), std::string("sub")) << instr->mnemonic;
+  EXPECT_EQ(std::string(instr->op_str), std::string("esp, 0x70")) << instr->op_str;
+}
 
-    // decl %eax
-    // subl $0x70, %esp
-    const char *code = "\x48\x83\xec\x70";
-    auto res = dis.disassemble(QByteArray(code));
-    ASSERT_NE(res, nullptr);
-    EXPECT_EQ(res->count(), std::size_t(2));
+TEST(Disassembler, disassembleData_Intel_64_x86)
+{
+  auto obj = std::make_unique<BinaryObject>();
+  obj->setCpuType(CpuType::X86_64);
 
-    auto *instr = res->instructions(0);
-    ASSERT_NE(instr, nullptr);
-    EXPECT_EQ(std::string(instr->mnemonic), std::string("decl")) << instr->mnemonic;
-    EXPECT_EQ(std::string(instr->op_str), std::string("%eax")) << instr->op_str;
+  Disassembler dis(*obj.get(), Disassembler::Syntax::INTEL);
+  ASSERT_TRUE(dis.valid());
 
-    instr = res->instructions(1);
-    ASSERT_NE(instr, nullptr);
-    EXPECT_EQ(std::string(instr->mnemonic), std::string("subl")) << instr->mnemonic;
-    EXPECT_EQ(std::string(instr->op_str), std::string("$0x70, %esp")) << instr->op_str;
-  }
+  // sub rsp, 0x70
+  const char *code = "\x48\x83\xec\x70";
+  auto res = dis.disassemble(QByteArray(code));
+  ASSERT_NE(res, nullptr);
+  EXPECT_EQ(res->count(), std::size_t(1));
 
-  {
-    // x86 64-bit
-    auto obj = std::make_unique<BinaryObject>();
-    obj->setCpuType(CpuType::X86_64);
+  auto *instr = res->instructions(0);
+  ASSERT_NE(instr, nullptr);
+  EXPECT_EQ(std::string(instr->mnemonic), std::string("sub")) << instr->mnemonic;
+  EXPECT_EQ(std::string(instr->op_str), std::string("rsp, 0x70")) << instr->op_str;
+}
 
-    Disassembler dis(*obj.get(), Disassembler::Syntax::INTEL);
-    ASSERT_TRUE(dis.valid());
+TEST(Disassembler, disassembleData_Att_32_x86)
+{
+  auto obj = std::make_unique<BinaryObject>();
+  obj->setCpuType(CpuType::X86);
 
-    // sub rsp, 0x70
-    const char *code = "\x48\x83\xec\x70";
-    auto res = dis.disassemble(QByteArray(code));
-    ASSERT_NE(res, nullptr);
-    EXPECT_EQ(res->count(), std::size_t(1));
+  Disassembler dis(*obj.get(), Disassembler::Syntax::ATT);
+  ASSERT_TRUE(dis.valid());
 
-    auto *instr = res->instructions(0);
-    ASSERT_NE(instr, nullptr);
-    EXPECT_EQ(std::string(instr->mnemonic), std::string("sub")) << instr->mnemonic;
-    EXPECT_EQ(std::string(instr->op_str), std::string("rsp, 0x70")) << instr->op_str;
-  }
+  // decl %eax
+  // subl $0x70, %esp
+  const char *code = "\x48\x83\xec\x70";
+  auto res = dis.disassemble(QByteArray(code));
+  ASSERT_NE(res, nullptr);
+  EXPECT_EQ(res->count(), std::size_t(2));
 
-  {
-    // x86 64-bit
-    auto obj = std::make_unique<BinaryObject>();
-    obj->setCpuType(CpuType::X86_64);
+  auto *instr = res->instructions(0);
+  ASSERT_NE(instr, nullptr);
+  EXPECT_EQ(std::string(instr->mnemonic), std::string("decl")) << instr->mnemonic;
+  EXPECT_EQ(std::string(instr->op_str), std::string("%eax")) << instr->op_str;
 
-    Disassembler dis(*obj.get(), Disassembler::Syntax::ATT);
-    ASSERT_TRUE(dis.valid());
+  instr = res->instructions(1);
+  ASSERT_NE(instr, nullptr);
+  EXPECT_EQ(std::string(instr->mnemonic), std::string("subl")) << instr->mnemonic;
+  EXPECT_EQ(std::string(instr->op_str), std::string("$0x70, %esp")) << instr->op_str;
+}
 
-    // subq $0x70, %rsp
-    const char *code = "\x48\x83\xec\x70";
-    auto res = dis.disassemble(QByteArray(code));
-    ASSERT_NE(res, nullptr);
-    EXPECT_EQ(res->count(), std::size_t(1));
+TEST(Disassembler, disassembleData_Att_64_x86)
+{
+  auto obj = std::make_unique<BinaryObject>();
+  obj->setCpuType(CpuType::X86_64);
 
-    auto *instr = res->instructions(0);
-    ASSERT_NE(instr, nullptr);
-    EXPECT_EQ(std::string(instr->mnemonic), std::string("subq")) << instr->mnemonic;
-    EXPECT_EQ(std::string(instr->op_str), std::string("$0x70, %rsp")) << instr->op_str;
-  }
+  Disassembler dis(*obj.get(), Disassembler::Syntax::ATT);
+  ASSERT_TRUE(dis.valid());
 
-  {
-    auto obj = std::make_unique<BinaryObject>();
-    Disassembler dis(*obj.get());
-    ASSERT_TRUE(dis.valid());
+  // subq $0x70, %rsp
+  const char *code = "\x48\x83\xec\x70";
+  auto res = dis.disassemble(QByteArray(code));
+  ASSERT_NE(res, nullptr);
+  EXPECT_EQ(res->count(), std::size_t(1));
 
-    auto res = dis.disassemble(QByteArray(nullptr));
-    ASSERT_EQ(res, nullptr);
-  }
+  auto *instr = res->instructions(0);
+  ASSERT_NE(instr, nullptr);
+  EXPECT_EQ(std::string(instr->mnemonic), std::string("subq")) << instr->mnemonic;
+  EXPECT_EQ(std::string(instr->op_str), std::string("$0x70, %rsp")) << instr->op_str;
+}
+
+TEST(Disassembler, disassembleNoData)
+{
+  auto obj = std::make_unique<BinaryObject>();
+  Disassembler dis(*obj.get());
+  ASSERT_TRUE(dis.valid());
+
+  auto res = dis.disassemble(QByteArray(nullptr));
+  ASSERT_EQ(res, nullptr);
 }
 
 TEST(Disassembler, disassembleText)
 {
-  {
-    auto obj = std::make_unique<BinaryObject>();
-    Disassembler dis(*obj.get());
-    ASSERT_TRUE(dis.valid());
+  auto obj = std::make_unique<BinaryObject>();
+  Disassembler dis(*obj.get());
+  ASSERT_TRUE(dis.valid());
 
-    auto res = dis.disassemble(QString("90 90 90"));
-    ASSERT_NE(res, nullptr);
-    ASSERT_EQ(res->count(), std::size_t(3));
+  auto res = dis.disassemble(QString("90 90 90"));
+  ASSERT_NE(res, nullptr);
+  ASSERT_EQ(res->count(), std::size_t(3));
 
-    for (std::size_t i = 0; i < res->count(); i++) {
-      auto *instr = res->instructions(i);
-      ASSERT_NE(instr, nullptr);
-      EXPECT_EQ(std::string(instr->mnemonic), std::string("nop")) << instr->mnemonic;
-    }
+  for (std::size_t i = 0; i < res->count(); i++) {
+    auto *instr = res->instructions(i);
+    ASSERT_NE(instr, nullptr);
+    EXPECT_EQ(std::string(instr->mnemonic), std::string("nop")) << instr->mnemonic;
   }
 }
