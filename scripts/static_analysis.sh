8db4582db0856171dabef023488f7d33a3f51207
Morten Kristensen <me@mortens.dev>
2020-03-11 19:19:45 +0100

    [sa] Exclude third party libs from analysis

--- scripts/static_analysis.sh
+++ scripts/static_analysis.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 SCAN_BUILD=$1
-BUILD_FLD=$2
-QT_DIR=$3
-TARGET=$4
+SRC_FLD=$2
+BUILD_FLD=$3
+QT_DIR=$4
+TARGET=$5
+
+set -x
 
 # Hack to make ccc-analyzer find Qt includes properly!
 rm -f ${BUILD_FLD}/src/QtCore ${BUILD_FLD}/src/QtGui ${BUILD_FLD}/src/QtWidgets
@@ -10,4 +13,6 @@ ln -s ${QT_DIR}/QtCore.framework/Headers/ ${BUILD_FLD}/src/QtCore
 ln -s ${QT_DIR}/QtGui.framework/Headers/ ${BUILD_FLD}/src/QtGui
 ln -s ${QT_DIR}/QtWidgets.framework/Headers/ ${BUILD_FLD}/src/QtWidgets
 
-${SCAN_BUILD} -v -V -k cmake --build ${BUILD_FLD} --target ${TARGET}
+${SCAN_BUILD} -v -V -k \
+  --exclude ${SRC_FLD}/lib/ --exclude src/QtCore --exclude src/QtGui --exclude src/QtWidgets \
+  cmake --build ${BUILD_FLD} --target ${TARGET}
