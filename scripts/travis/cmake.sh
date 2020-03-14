a0dc24be7ad0e0e11c647a1d31e17467c920b287
Morten Kristensen <me@mortens.dev>
2020-03-14 23:31:44 +0100

    [travis] Enable ASAN with Clang on Linux

--- scripts/travis/cmake.sh
+++ scripts/travis/cmake.sh
@@ -3,8 +3,8 @@ cmake --system-information info.txt && cat info.txt
 mkdir build
 cd build
 
-# Use address sanitizer with Clang on macOS.
-if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+# Use address sanitizer with Clang on macOS and Linux.
+if [ "$CXX" == "clang++" ] || [ "$CXX" == "clang++-9" ]; then
   cmake -DBUILD_TESTS=ON -DADDRESS_SANITIZER=ON ..
 else
   cmake -DBUILD_TESTS=ON ..
