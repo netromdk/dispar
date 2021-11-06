783ab100d57d0a2d705abc63d16d5e6e32b32239
Morten Kristensen <me@mortens.dev>
2021-11-06 09:58:12 +0100

    [actions] Test on macOS and Linux

---
+++ scripts/actions/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Be verbose.
+set -x
+
+UNAME=$(command -v uname)
+printf 'uname=${UNAME}\n'
+case $("${UNAME}" | tr '[:upper:]' '[:lower:]') in
+  linux*)
+    sudo apt-get install -qq ninja-build mesa-common-dev libglu1-mesa-dev
+    ;;
+  darwin*)
+    brew install -v ninja
+    ;;
+  msys*|cygwin*|mingw*|nt|win*)
+    printf 'windows\n'
+    ;;
+esac
