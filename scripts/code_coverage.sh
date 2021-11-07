6f6e2604a5a709995d5959166d1b42190c6cb739
Morten Kristensen <me@mortens.dev>
2021-11-07 15:00:36 +0100

    Fixed coverage script to support analyzing several binaries

--- scripts/code_coverage.sh
+++ scripts/code_coverage.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 DIR=$1
-BINARY_FILE=$2
-LLVM_PROFDATA=$3
-LLVM_COV=$4
-LLVM_PROFILE_FILE=default.profraw
+LLVM_PROFDATA=$2
+LLVM_COV=$3
 LLVM_PROFILE_DATA=tests.profdata
 REPORT_DIR=report.dir
 
 pushd ${DIR}
-rm -fr ${REPORT_DIR}
-"${BINARY_FILE}" >/dev/null && \
-    ${LLVM_PROFDATA} merge -sparse ${LLVM_PROFILE_FILE} -o ${LLVM_PROFILE_DATA} && \
-    ${LLVM_COV} show "${BINARY_FILE}" -instr-profile=${LLVM_PROFILE_DATA} -format html -o ${REPORT_DIR} -Xdemangler c++filt -Xdemangler -n -show-line-counts-or-regions && \
-    ${LLVM_COV} report "${BINARY_FILE}" -instr-profile=${LLVM_PROFILE_DATA} -Xdemangler c++filt -Xdemangler -n -use-color && \
-    echo "============================\nReport at: ${REPORT_DIR}/index.html\n============================"
+rm -fr ${REPORT_DIR} *.profraw *.profdata
+
+# Generate profiling data from all tests executables.
+for f in $(find "${DIR}/bin" -iname 'test_*'); do
+  LLVM_PROFILE_FILE="$(basename $f).profraw" $f >/dev/null 2>/dev/null
+  COV_BINARIES="${COV_BINARIES} -object $f"
+done
+
+# Merge all raw profile data into one data file.
+${LLVM_PROFDATA} merge -sparse *.profraw -o ${LLVM_PROFILE_DATA}
+
+# Generate HTML report.
+${LLVM_COV} show -instr-profile=${LLVM_PROFILE_DATA} -format html -o ${REPORT_DIR}\
+            -Xdemangler c++filt -Xdemangler -n -show-line-counts-or-regions\
+            -ignore-filename-regex="(build|tests)/*"\
+            ${COV_BINARIES}
+
+echo "===========================\nReport at: ${REPORT_DIR}/index.html\n==========================="
 popd
