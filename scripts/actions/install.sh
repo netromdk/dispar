008a4f0cf9fa2542f49c82d3ec4ffa7f727e1092
Morten Kristensen <me@mortens.dev>
2021-11-20 11:05:26 +0100

    WIP: update apt to fix github actions

--- scripts/actions/install.sh
+++ scripts/actions/install.sh
@@ -7,6 +7,7 @@ UNAME=$(command -v uname)
 printf 'uname=${UNAME}\n'
 case $("${UNAME}" | tr '[:upper:]' '[:lower:]') in
   linux*)
+    sudo apt-get update
     sudo apt-get install -qq ninja-build mesa-common-dev libglu1-mesa-dev
     ;;
   darwin*)
